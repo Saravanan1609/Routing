921bed71f0350f1215ddade8b6fda34c1bd8d2b3
Saravanan.P <Saravanan.P@iinterchange.net>
2022-02-21 17:04:51 +0530

    First commit

--- src/app/user-create/user-create.component.ts
+++ src/app/user-create/user-create.component.ts
@@ -1,40 +1,60 @@
 import { Component, OnInit } from '@angular/core';
 import { FormArray, FormControl,FormGroup,Validators } from "@angular/forms";
+import { ActivatedRoute, Router } from '@angular/router';
 import { UserService } from '../user.service';
-import { Router } from '@angular/router';
 @Component({
   selector: 'app-user-create',
   templateUrl: './user-create.component.html',
   styleUrls: ['./user-create.component.css']
 })
 export class UserCreateComponent implements OnInit {
-userFormData:any;
-isPosting : boolean = false;
+  userFormData:any;
+  isPosting : boolean = false;
   constructor(private userService:UserService,private router:Router) { 
     this.userFormData = new FormGroup({
-          'username' : new FormControl("",[Validators.required,Validators.minLength(3),Validators.maxLength(10)]),
-          'email' : new FormControl("",[Validators.required,Validators.email]),
-          'phone' : new FormControl("",[Validators.required,Validators.min(6),Validators.max(9999999999), Validators.pattern(/^[6-9]\d*$/) ],),
-          'countryCode' : new FormControl(""),
-          'country' : new FormControl(),
-          'state' : new FormControl(),
-          'city' : new FormControl(),          
+      'username' : new FormControl("",[Validators.required,Validators.minLength(3),Validators.maxLength(15)]),
+      'email' : new FormControl("",[Validators.required,Validators.email]),
+      'phone' : new FormControl("",[Validators.required,Validators.min(9999999),Validators.max(999999999)]),
+      'countryCode' : new FormControl(""),
+      'country' : new FormControl("IN"),
+      'state' : new FormControl("TN"),
+      'city' : new FormControl("CH"),
+      'skills' : new FormArray([
+        new FormGroup({
+          'skillName' : new FormControl("",Validators.required),
+          'ratting' : new FormControl()
+        }),
+        new FormGroup({
+          'skillName' : new FormControl("",Validators.required),
+          'ratting' : new FormControl()
+        }),
+        new FormGroup({
+          'skillName' : new FormControl("",Validators.required),
+          'ratting' : new FormControl()
+        }),
+        new FormGroup({
+          'skillName' : new FormControl("",Validators.required),
+          'ratting' : new FormControl()
+        }),
+      ])
     })
   }
+
   ngOnInit(): void {
   }
 
-  postFormData() {
+  postFormData(){
     if(this.userFormData.valid){
       this.isPosting = true;
-      this.userService.createUser(this.userFormData.value).subscribe((data)=>{
+      this.userService.createUser(this.userFormData.value).subscribe((data) => {
         this.isPosting = false;
-        this.router.navigate(["/dashboard/user/list"])      
+          this.router.navigate(["/dashboard/user/list"])
       },(error) => {
-        this.isPosting = true;
-        alert("data errored");        
+        this.isPosting = false;
+        alert("Error")
       })
     }
   }
 
-} 
+}
+
